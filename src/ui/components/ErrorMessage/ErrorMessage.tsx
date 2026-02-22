ac2d2c8c98d5d9eaed6f98343a3c83505482d238
Kuncheria <kunchjose3@gmail.com>
2026-02-22 15:37:16 +1100

    create error component

---
+++ src/ui/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+
+import $ from "./ErrorMessage.module.css";
+
+interface ErrorMessageProps {
+  message: string;
+}
+
+const ErrorMessage: FunctionComponent<ErrorMessageProps> = ({ message }) => {
+  return <div className={$.error}>{message}</div>;
+};
+
+export default ErrorMessage;
